ef3a4138b7dd170a43da473ce8812d17955b86d7
RayTechEnthusiast <tru3parzival@gmail.com>
2025-12-17 01:12:58 +0000

    YAY DONE YIPEEEEEEEE

--- src/main/java/com/BrickBreak/ Paddle.java
+++ src/main/java/com/BrickBreak/ Paddle.java
@@ -1,21 +1,86 @@
-package com.BrickBreak;
+// Class author: Rayan Hashmi
+// Date created: 12/16/25
+// General description: It's the paddle, controlled by the player, in Brick Break.
+// The paddle moves left to right and is controlled via keyboard
 
-import java.awt.Graphics;
+
+package com.BrickBreak;
 import java.awt.*;
 
 public class Paddle {
 
+    // Instance variables
+    private int x;
+    private int y;
+	private int vel; //velocity
+    private int width;
+    private int height;
 
-	//your code here!
+    // Pre-condition: variables x, y, width, and height must be valid screen values
+    // Post-condition: Paddle created at given pos with no movement
+    
+    public Paddle(int x, int y, int width, int height) {
+       
+		this.width = width;
+        this.height = height;
+		this.x = x;
+        this.y = y;
+        vel = 0;
+    }
+
+    
+    // Pre-condition: none
+    // Post-condition: Paddle moves left to right
+    
+    public void move() {
+        x += vel;
+    }
+
+    
+    // Pre-condition: Graphics object not null
+    // Post-condition: Paddle drawn on screen
+    
+    public void draw(Graphics g) {
+
+        g.setColor(Color.YELLOW);
+        g.fillRect(x, y, width, height);
+
+    }
+
+    
+    // Pre-condition: v is valid velocity value
+    // Post-condition: Paddle velocity set to v
+    
+    public void addVelocity(int v) {
+        vel = v; 
+    }
 
 
-	
-	//don't forget you need instance variables:
-	
-	
-	//constructor(s):
-	
-	//methods:
-	
-	
+    public int getX() {
+        return x;
+    }
+    
+    public int getY() {
+        return y;
+    }
+    
+    public int getWidth() {
+        return width;
+    }
+    
+    public int getHeight() {
+        return height;
+    }
+
+    public int getVelocity() {
+        return vel;
+    }
+    
+    public void setVelocity(int v) {
+        vel = v;
+    }
+    
+    public void setX(int x) {
+        this.x = x;
+    }
 }
--- src/main/java/com/BrickBreak/Ball.java
+++ src/main/java/com/BrickBreak/Ball.java
@@ -1,25 +1,93 @@
-//Your name
-// a bief description
+
+// Class author: Rayan Hashmi
+// Date created: 12/16/25
+// General description: Class that manages ball in the game.
 
 package com.BrickBreak;
 import java.awt.*;
 
 public class Ball {
-	//your code here!
-	
-	//don't forget you need instance variables:
-	private int x;
-	private int y;
-	private int size;
-	
-	
-	//constructor(s):
-	
-	//methods:
-	public void draw(Graphics g){
-		g.setColor(Color.cyan);
-		g.fillOval(x,y,size,size);
-		
 
-	}
+    // Instance variables
+    private int x;
+    private int y;
+    private int dx;
+    private int dy;
+	private int size;
+
+    
+    // Pre-condition: variables x y and size must be valid screen values
+    // Post-condition: Ball object created with starting velocity
+    
+    public Ball(int x, int y, int size) {
+        
+		this.x = x;
+        this.y = y;
+        this.size = size;
+        dx = 1;
+        dy = -2;
+
+    }
+
+    
+    // Pre-condition: none
+    // Post-condition: Ball position changed based on velocity
+    
+    public void move() {
+        x = x + dx;
+        y += dy;
+    }
+
+    
+    // Pre-condition: Graphics object not null
+    // Post-condition: Ball drawn (on screen)
+    
+    public void draw(Graphics g) {
+        g.setColor(Color.BLUE);
+        g.fillOval(x, y, size, size);
+    }
+
+    
+    // Pre-condition: none
+    // Post-condition: Up down (vertical) direction of ball reversed
+    
+    public void reverseY() {
+        dy = -dy;
+    }
+
+    
+    // Pre-condition: none
+    // Post-condition: Left right (horizontal) direction of ball reversed
+    
+    public void reverseX() {
+        dx = -dx;
+    }
+
+    public int getXpos() {
+        return x;
+    }
+    
+    public int getYpos() {
+        return y;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+    
+    public void setY(int y) {
+        this.y = y;
+    }
+    
+    public void setXVelocity(int dx) {
+        this.dx = dx;
+    }
+
+    public void setYVelocity(int dy) {
+        this.dy = dy;
+    }
 }
